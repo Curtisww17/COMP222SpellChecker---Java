d7482e593a8adec32cd658901173e9c01e76c6a7
Curtisww17 <wcvikingdude@gmail.com>
2019-03-23 17:04:27 -0400

    Wrote methods in the SpellingTree class

--- src/SpellingTree.java
+++ src/SpellingTree.java
@@ -16,7 +16,20 @@ public class SpellingTree {
 	 * @return false if the word was already in the tree
 	 */
 	public boolean addWord(String word) {
-		return false;
+		if (checkWord(word))
+			return false;
+		char[] chars = word.toCharArray();
+		SpellingNode currentNode = root;
+		for (int i = 0; i < chars.length; i++) {
+			if (checkChar(chars[i],currentNode))
+				currentNode = currentNode.getChildAt(chars[i]);
+			else {
+				currentNode.addChild(chars[i]);
+				currentNode = currentNode.getChildAt((chars[i]));
+			}
+		}
+		currentNode.setCorrect();
+		return true;
 	}
 	
 	/**
@@ -25,7 +38,16 @@ public class SpellingTree {
 	 * @return true if the word is in the tree
 	 */
 	public boolean checkWord(String word) {
-		return false;
+		SpellingNode currentNode = root;
+		char[]  chars = word.toCharArray();
+		for (int i = 0; i < chars.length; i++) {
+			if (checkChar(chars[i], currentNode))
+				currentNode = currentNode.getChildAt(chars[i]);
+			else return false;
+		}
+		if (currentNode.getCorrect())
+			return true;
+		else return false;
 	}
 	
 	/**
@@ -34,6 +56,25 @@ public class SpellingTree {
 	 * @param c this is the current relative root of the subtree
 	 */
 	public void printWords(String subWord, SpellingNode c) {
-		
+		String word = subWord + Character.toString(c.value);
+		if (c.getCorrect())
+			System.out.println(word);
+		for (int i = 0; i < 26; i++) {
+			char currentChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(i);
+			if (c.getChildAt(currentChar) != null)
+				printWords(word,c.getChildAt(currentChar));
+		}
+	}
+	
+	/**
+	 * 
+	 * @param val the character checking for
+	 * @param c the Node of which we check its children
+	 * @return true if val is in the children of c
+	 */
+	private boolean checkChar(char val, SpellingNode c) {
+		if (c.getChildAt(val) == null)
+			return false;
+		else return true;
 	}
 }
